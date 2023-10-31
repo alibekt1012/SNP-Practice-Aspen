50ac9e79f26b75ab08ef76ab7d9034eae99891ea
alibekt1012 <alibekt1012@gmail.com>
2023-10-31 12:15:56 +0600

    Initial Commit

---
+++ Aspen Vacation Snapkit Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Aspen Vacation Snapkit Challenge
+//
+//  Created by Almat Alibekov on 31.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
