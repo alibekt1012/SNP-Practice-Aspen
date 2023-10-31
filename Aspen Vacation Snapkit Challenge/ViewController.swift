79ebf2dcb75ab75bd8b1af6e0852fc76d5a25b71
alibekt1012 <alibekt1012@gmail.com>
2023-10-31 17:31:03 +0600

    - Remove Main StoryBoard - Add Snapkit library - Added some plain and simple UI elements using snapkit into ViewController

--- Aspen Vacation Snapkit Challenge/ViewController.swift
+++ Aspen Vacation Snapkit Challenge/ViewController.swift
@@ -6,12 +6,108 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
+    // UI elements
+    
+    var titleAspen: UILabel = {
+        var label = UILabel()
+        label.text = "Aspen"
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = UIFont(name: "Hiatus", size: 116)
+        return label
+    }()
+    
+    var subTitle1: UILabel = {
+        var label = UILabel()
+        label.text = "Plan your"
+        label.textColor = .white
+        label.font = UIFont(name: "Montserrat", size: 30)
+        return label
+    }()
+    
+    var subTitle2: UILabel = {
+        var label = UILabel()
+        label.text = "Luxurious Vacation"
+        label.textColor = .white
+        label.font = UIFont(name: "Montserrat", size: 50)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    var background: UIImageView = {
+       
+        var image = UIImageView()
+        image.image = UIImage(named: "aspen_bc")
+        return image
+        
+    }()
+    
+    var exploreButton: UIButton = {
+       
+        var button = UIButton()
+        button.setTitle("Explore", for: .normal)
+        button.titleLabel?.font = UIFont(name: "Montserrat", size: 16)
+        button.backgroundColor = UIColor(red: 0.10, green: 0.43, blue: 0.93, alpha: 1.00)
+        button.layer.cornerRadius = 16
+        return button
+        
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupViews()
+        setupConstraints()
+    }
+    
+    // Setup Views
+    
+    func setupViews() {
+        view.addSubview(background)
+        view.addSubview(titleAspen)
+        view.addSubview(subTitle1)
+        view.addSubview(subTitle2)
+        view.addSubview(exploreButton)
+        
+    }
+    
+    // Setup Constraints
+    func setupConstraints() {
+        background.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.trailing.equalToSuperview()
+        }
+        
+        titleAspen.snp.makeConstraints { make in
+            make.top.equalTo(93)
+            make.leading.equalTo(57)
+            make.trailing.equalTo(-55)
+        }
+        
+        subTitle1.snp.makeConstraints { make in
+            make.leading.equalTo(32)
+            make.top.equalTo(titleAspen.snp.bottom).offset(334)
+        }
+        
+        subTitle2.snp.makeConstraints { make in
+            make.top.equalTo(subTitle1.snp.bottom).offset(8)
+            make.leading.equalTo(32)
+            make.trailing.equalTo(48)
+        }
+        
+        exploreButton.snp.makeConstraints { make in
+            make.leading.equalTo(32)
+            make.top.equalTo(subTitle2.snp.bottom).offset(24)
+            make.height.equalTo(52)
+            make.trailing.equalTo(-32)
+        }
     }
 
 
